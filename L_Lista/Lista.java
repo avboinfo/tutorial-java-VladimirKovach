377857077eb9b2d0aa28312183cb6638a47f6f80
VladimirKovach <150423755+VladimirKovach@users.noreply.github.com>
2024-03-14 11:24:34 +0000

    lista

---
+++ L_Lista/Lista.java
@@ -0,0 +1,35 @@
+package L_Lista;
+
+public class Lista {
+
+    Nodo radice;
+    
+    public Lista() {
+        radice = null;
+    }
+
+    public boolean isEmpty() {
+        return radice==null;
+    }
+
+    public void add( Nodo n ) {
+        if (isEmpty()) {
+            radice = n;
+        } else {
+            Nodo p = radice;
+            while (p.getSuccessivo()!=null) p = p.getSuccessivo();
+            p.setSuccessivo( n );
+        }
+    }
+
+    public String toString() {
+        String s = "Elementi della lista: ";
+        Nodo p = radice;
+        while (p!=null) {
+            s += p + " ";
+            p = p.getSuccessivo();
+        }
+        s += "End!\n";
+        return s;
+    }
+}
